3786cdf01b33061c5a59c4fe874c83ff1e92c6a4
hobbydevs <hart_herbert@hotmail.com>
2020-09-18 21:48:19 +0200

    added gcloud authentication

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
-import {exec} from 'child_process';
+import {exec, execSync} from 'child_process';
 import semverGt from 'semver/functions/gt';
 import semverCoerce from 'semver/functions/coerce';
 import {IServiceVersion} from './common';
@@ -10,13 +10,42 @@ const appToSemverVersion = (version: string) => {
   return semverCoerce(String(version).split('-').join('.'));
 };
 
+const authenticateGCloudCli = (
+  projectId: string,
+  credentials: string
+): void => {
+  const isBase64 = (str: string): boolean => {
+    const base64Regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    return !!str && base64Regex.test(str);
+  };
+
+  const writeData = isBase64(credentials)
+    ? Buffer.from(credentials, 'base64')
+    : credentials;
+
+  fs.writeFileSync('/tmp/account.json', writeData, {encoding: 'utf8'});
+
+  // authenticate
+  execSync(`gcloud auth activate-service-account --key-file=/tmp/account.json`);
+  // set project
+  execSync(`gcloud config set project "${projectId}"`);
+};
+
 async function run(): Promise<void> {
   try {
+    // variables
     const currentSemverVersion = appToSemverVersion(
       core.getInput('current_version')
     );
-
     const appYamlFilePath = core.getInput('app_yaml_file_path');
+    // environment variables
+    const projectId = String(process.env.GCLOUD_PROJECT_ID);
+    const applicationCredentials = String(
+      process.env.GCLOUD_APPLICATION_CREDENTIALS
+    );
+
+    authenticateGCloudCli(projectId, applicationCredentials);
+
     let serviceName = 'default';
 
     const gcpAppConfig = yaml.safeLoad(
